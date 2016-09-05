714311d10d3d1bd84f469a7c534ec4e524c2e1ac
Nick Sablukov <Dessanndes@gmail.com>
2016-09-05 11:54:26 +0300

    Обход предков и полкчение аргументов , юзал нутря, чтоб пахало для 3 и для 2

--- dyn_struct/datatools.py
+++ dyn_struct/datatools.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+
 import django.forms
 from dyn_struct.exceptions import CheckClassArgumentsException
 
@@ -11,8 +12,26 @@ def get_django_widgets():
     return django.forms.widgets.__all__
 
 
-def check_class_arguments(class_obj, kwargs):
-    available_arg_names = class_obj.__init__.__code__.co_varnames
+def get_all_bases_classes(class_obj, base_classes=None):
+    if base_classes is None:
+        base_classes = [class_obj, ]
+
+    bases = [base_class for base_class in class_obj.__bases__ if base_class is not object]
+
+    if bases:
+        for base_class in class_obj.__bases__:
+            if base_class != object:
+                base_classes.extend(get_all_bases_classes(base_class, bases))
+
+    return base_classes
+
+
+def check_class_arguments(cls, kwargs):
+    classes = get_all_bases_classes(cls)
+
+    classes_params = [class_obj.__init__.__code__.co_varnames for class_obj in classes]
+    available_arg_names = set([param for param in classes_params if param not in ['args', 'kwargs', 'self']])
+
     kwargs_names = set(kwargs.keys())
 
     error_keys = kwargs_names - available_arg_names
