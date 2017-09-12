51feaca73f81bfb867d37e2cfdf2b902e342fdca
des <dessanndes@gmail.com>
2017-09-12 14:18:34 +0300

    #4 has created manage.py commands to dump and loads structs info

---
+++ dyn_struct/management/commands/dump_dynamic_structure.py
@@ -0,0 +1,58 @@
+import json
+
+from django.core.management.base import BaseCommand
+
+from dyn_struct.db import models
+
+
+class Command(BaseCommand):
+    help = 'Dump dynamic structure'
+
+    def add_arguments(self, parser):
+        parser.add_argument('-i', '--indent', dest='indent', type=int)
+        parser.add_argument('-s', '--sorted', dest='sorted', default=False, action='store_true')
+        parser.add_argument('-p', '--pretty', dest='pretty', default=False, action='store_true')
+
+    def handle(self, *args, **options):
+        structures = models.DynamicStructure.objects.filter(is_deprecated=False)
+        structures_data = []
+
+        for struct in structures.iterator():
+            structures_data.append({
+                'name': struct.name,
+                'version': struct.version,
+                'fields': self.get_fields_data(struct)
+            })
+
+        pretty = options['pretty']
+        dump_kwargs = {
+            'sort_keys': options['sorted']
+        }
+        if options['indent'] is not None:
+            dump_kwargs['indent'] = options['indent']
+
+        if pretty:
+            dump_kwargs['ensure_ascii'] = False
+
+        dumped_data = json.dumps(structures_data, **dump_kwargs)
+        if pretty:
+            dumped_data = dumped_data.encode('utf-8').decode('utf-8')
+
+        print(dumped_data)
+
+    def get_fields_data(self, struct):
+        fields_data = []
+        for field in struct.fields.all():
+            fields_data.append({
+                'header': field.header,
+                'name': field.name,
+                'form_field': field.form_field,
+                'form_kwargs': field.form_kwargs,
+                'widget': field.widget,
+                'widget_kwargs': field.widget_kwargs,
+                'row': field.row,
+                'position': field.position,
+                'classes': field.classes,
+            })
+
+        return fields_data
