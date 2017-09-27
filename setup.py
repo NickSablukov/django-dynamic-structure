4239397d842f17634a8af371d0a83dc7f115ba12
des <dessanndes@gmail.com>
2017-09-27 21:50:07 +0300

    template fix

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-dynamic-structure',
-    version='0.0.17',
+    version='0.0.18',
     description='Dynamical django model structure. For example, for user customized medical specialist protocols.',
     author='Telminov Sergey',
     author_email='sergey@telminov.ru',
     url='https://github.com/NickSablukov/django-dynamic-structure',
     include_package_data=True,
     packages=find_packages(),
+    package_data={'dyn_struct': ['templates/dyn_struct/*.html']},
     license='The MIT License',
     install_requires=[
         'django', 'six', 'sw-python-utils',
